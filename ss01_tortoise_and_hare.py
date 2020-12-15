d61c45089806fa971e8168d19a4d85367da05167
Le Nguyen <74449922+ELIHT@users.noreply.github.com>
2020-12-15 23:41:35 +0700

    Add files via upload

---
+++ ss01_tortoise_and_hare.py
@@ -0,0 +1,27 @@
+#simple simulation: the tortoise and the hare
+
+tortoise_speed = 1
+hare_speed = 2
+race_distance = 350
+mins_between_naps = 30
+nap_length = 20
+
+tortoise_pos = 0
+hare_pos = 0
+mins_since_last_nap = 0
+mins_since_nap_start = 0
+
+while tortoise_pos < race_distance and hare_pos < race_distance:
+    tortoise_pos += tortoise_speed
+    if mins_since_last_nap <= mins_between_naps:
+        hare_pos += hare_speed
+        mins_since_last_nap += 1
+    else:
+        if mins_since_nap_start < nap_length:
+            mins_since_nap_start += 1
+        else:
+            mins_since_last_nap = 0
+            mins_since_nap_start = 0
+
+print("Tortoise position: %d" %tortoise_pos)
+print("Hare position: %d" %  hare_pos)
